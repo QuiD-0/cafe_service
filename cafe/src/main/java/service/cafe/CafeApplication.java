01d78e488609d7962de607457d2fc52d58a20db7
QuiD <wodnd101@naver.com>
2021-11-09 21:55:50 +0900

    setting

--- cafe/src/main/java/service/cafe/CafeApplication.java
+++ cafe/src/main/java/service/cafe/CafeApplication.java
@@ -1,4 +1,4 @@
-package JU.cafe;
+package service.cafe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- cafe/src/test/java/service/cafe/CafeApplicationTests.java
+++ cafe/src/test/java/service/cafe/CafeApplicationTests.java
@@ -1,4 +1,4 @@
-package JU.cafe;
+package service.cafe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
